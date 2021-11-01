75a879effe9cee256aa954f56cbc4f1a57483c91
umbranoxio <itsumbra@protonmail.com>
2021-11-02 01:12:40 +1100

    Add caching and event based revalidation

--- src/lib/utils/accio/index.ts
+++ src/lib/utils/accio/index.ts
@@ -1,5 +1,7 @@
 import { onMount, onDestroy } from 'svelte';
+import { CACHE_EXPIRY_IN_MINUTES } from '$lib/utils/env';
 import { writable } from 'svelte/store';
+import { DefaultCache, CacheItem } from './cache';
 import queryString from 'query-string';
 
 export class Accio {
@@ -13,17 +15,32 @@ export class Accio {
          await loadData(key);
       });
 
-      const refresh = async (refreshOptions?: Partial<AccioRefreshOptions<D>>) => {
-         data.set(null);
-         error.set(null);
+      const refresh = async (refreshOptions?: Partial<AccioRefreshOptions>) => {
          if (refreshOptions.query) {
             key = `${rootKey}${refreshOptions.query}`;
          }
+         if (!refreshOptions.softRefresh) {
+            data.set(null);
+            error.set(null);
+         }
          await loadData(key);
       };
+
       const loadData = async (key: string) => {
          try {
-            const rawData = await options.fetcher(key);
+            let rawData = undefined;
+            if (cache.has(key)) {
+               const cachedData = cache.get(key);
+               if (!cachedData.hasExpired()) {
+                  rawData = cache.get(key).data;
+               }
+            }
+            if (!rawData) {
+               rawData = await options.fetcher(key);
+               const expiry = new Date();
+               expiry.setTime(expiry.getTime() + parseInt(CACHE_EXPIRY_IN_MINUTES) * 60000);
+               cache.set(key, new CacheItem({ data: rawData, expiresAt: expiry }));
+            }
             data.set(rawData);
             if (options.dataLoaded) {
                options.dataLoaded(rawData);
@@ -32,30 +49,44 @@ export class Accio {
             error.set(ex);
          }
       };
+
+      if (typeof window !== 'undefined') {
+         let lastFocus: number | null = null;
+         const rawHandler = () => {
+            const now = Date.now();
+            if (lastFocus === null || now - lastFocus > 5000) {
+               lastFocus = now;
+               console.log('Refreshing for on focus');
+               refresh({ softRefresh: true });
+            }
+         };
+         window.addEventListener('focus', rawHandler);
+      }
+
+      if (typeof window !== 'undefined') {
+         window.addEventListener('online', () => {
+            console.log('User is back online, refreshing data');
+            refresh({ softRefresh: true });
+         });
+      }
       onDestroy(() => unsubscribe?.());
       return { data, error, refresh };
    }
 }
-
 export interface DataLoaded {
    (data: any): void;
 }
 
 export type AccioFetcher<D = any> = (...props: any[]) => Promise<D>;
 export interface AccioOptions<D = any> {
-   /**
-    * Determines the fetcher function to use.
-    */
    fetcher: AccioFetcher<D>;
    query: string;
    dataLoaded: DataLoaded;
 }
 
-export interface AccioRefreshOptions<D = any> {
-   /**
-    * Determines the fetcher function to use.
-    */
+export interface AccioRefreshOptions {
    query?: string;
+   softRefresh?: boolean;
 }
 
 const fetcher = <D>(url: string): Promise<D> => {
@@ -71,9 +102,10 @@ export const defaultOptions: AccioOptions = {
    dataLoaded: null
 };
 
-export const createSWR = <D = any>(options?: Partial<AccioOptions<D>>) => new Accio();
-export let swr = createSWR();
+export const createAccio = <D = any>(options?: Partial<AccioOptions<D>>) => new Accio();
+export let accio = createAccio();
+const cache = new DefaultCache();
 
 export const useAccio = <D = any, E = Error>(key: string, options?: Partial<AccioOptions<D>>) => {
-   return swr.useAccio<D, E>(key, options);
+   return accio.useAccio<D, E>(key, options);
 };
--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,2 +1,3 @@
 export const CDN_URL: string = import.meta.env.VITE_CDN_URL as string;
 export const API_URL: string = import.meta.env.VITE_API_URL as string;
+export const CACHE_EXPIRY_IN_MINUTES: string = import.meta.env.VITE_CACHE_EXPIRY_IN_MINUTES as string;
