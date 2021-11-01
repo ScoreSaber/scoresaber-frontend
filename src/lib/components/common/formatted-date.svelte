afb111319560d1547fbbf25769b69b7a7b33b663
umbranoxio <itsumbra@protonmail.com>
2021-11-02 02:06:30 +1100

    Add live date updating

--- src/lib/components/common/formatted-date.svelte
+++ src/lib/components/common/formatted-date.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
    import { format } from 'timeago.js';
    import dateFormat from 'dateformat';
+   import { onMount } from 'svelte';
    export let date: Date;
+
+   $: formattedDate = format(date);
+   $: hoverDate = dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+
+   onMount(() => {
+      const interval = setInterval(() => {
+         formattedDate = format(date);
+         hoverDate = dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+      }, 1000);
+      return () => {
+         clearInterval(interval);
+      };
+   });
 </script>
 
-<span class="date" title={dateFormat(date, 'dddd, mmmm dS, yyyy, h:MM:ss TT')}>{format(date)}</span>
+<span class="date" title={hoverDate}>{formattedDate}</span>
 
 <style>
    .date {
