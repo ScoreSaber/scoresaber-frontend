cc0b0121a9d8094fefc01cc9ce46decf0dc08b86
umbranoxio <itsumbra@protonmail.com>
2021-11-02 01:11:34 +1100

    Fix country image not updating properly

--- src/lib/components/image/country-image.svelte
+++ src/lib/components/image/country-image.svelte
@@ -2,7 +2,7 @@
    import { getCDNUrl } from '$lib/utils/helpers';
    import { getName } from 'country-list';
    export let country: string;
-   const countryImagePath = `/flags/${country.toLowerCase()}.png`;
+   $: countryImagePath = `/flags/${country.toLowerCase()}.png`;
 </script>
 
 <img alt={getName(country)} title={getName(country)} src={getCDNUrl(countryImagePath)} class="is-country" />
