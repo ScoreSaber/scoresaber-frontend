bfca666c1ba0ac9f8d2f39e99f9389a316ddfe31
JiveOff <antoine@jiveoff.fr>
2021-11-04 00:50:20 +0100

    tooling position changed

--- src/routes/ranking/request/[requestId].svelte
+++ src/routes/ranking/request/[requestId].svelte
@@ -104,7 +104,48 @@
                </div>
             </div>
             <div class="column is-4">
-               <div class="window has-shadow">
+               <div class="card map-card">
+                  <div
+                     class="bg-image"
+                     style={`background: linear-gradient(to left, rgba(36, 36, 36, 0.93), rgb(33, 33, 33)) repeat scroll 0% 0%, rgba(0, 0, 0, 0) url(${$request.leaderboardInfo.coverImage}) repeat scroll 0% 0%`}
+                  />
+                  <div class="card-content">
+                     <div class="media">
+                        <div class="media-content is-clipped">
+                           <div
+                              title={getDifficultyLabel($request.leaderboardInfo.difficulty)}
+                              class="tag mb-2 {getDifficultyStyle($request.leaderboardInfo.difficulty)}"
+                           >
+                              {getDifficultyLabel($request.leaderboardInfo.difficulty)}
+                           </div>
+                           <div class="title is-5 mb-0"><a href={'#'}>{$request.leaderboardInfo.songName}</a></div>
+                           <div class="subtitle is-6">by {$request.leaderboardInfo.songAuthorName}</div>
+                        </div>
+                        <div class="media-right">
+                           <figure class="image is-96x96 mr-0 ml-0">
+                              <img src={$request.leaderboardInfo.coverImage} alt="Map Cover" class="map-cover" />
+                           </figure>
+                        </div>
+                     </div>
+
+                     <div class="content">
+                        Mapped by <a href={'#'}><b>{$request.leaderboardInfo.levelAuthorName}</b></a><br />
+                        Status: <strong>{getRankingApprovalStatus($request.approved)}</strong><br />
+                        Request Type: <strong>{$request.requestType == 1 ? 'Rank' : 'Unrank'}</strong><br />
+                        <hr />
+                        <div class="votes">
+                           <div class="vote">RT 👍<br /><b>{$request.rankVotes.upvotes}</b></div>
+                           <div class="vote">RT 👎<br /><b>{$request.rankVotes.downvotes}</b></div>
+                        </div>
+                        <div class="votes">
+                           <div class="vote">QAT 👍<br /><b>{$request.qatVotes.upvotes}</b></div>
+                           <div class="vote">QAT 😐<br /><b>{$request.qatVotes.neutral}</b></div>
+                           <div class="vote">QAT 👎<br /><b>{$request.qatVotes.downvotes}</b></div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="window has-shadow mt-3">
                   <div class="title is-6 mb-3">Ranking Tool</div>
 
                   <div class="tooling mb-2">
@@ -192,47 +233,6 @@
                                  </span>
                               </button>
                            </p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="card map-card">
-                  <div
-                     class="bg-image"
-                     style={`background: linear-gradient(to left, rgba(36, 36, 36, 0.93), rgb(33, 33, 33)) repeat scroll 0% 0%, rgba(0, 0, 0, 0) url(${$request.leaderboardInfo.coverImage}) repeat scroll 0% 0%`}
-                  />
-                  <div class="card-content">
-                     <div class="media">
-                        <div class="media-content is-clipped">
-                           <div
-                              title={getDifficultyLabel($request.leaderboardInfo.difficulty)}
-                              class="tag mb-2 {getDifficultyStyle($request.leaderboardInfo.difficulty)}"
-                           >
-                              {getDifficultyLabel($request.leaderboardInfo.difficulty)}
-                           </div>
-                           <div class="title is-5 mb-0"><a href={'#'}>{$request.leaderboardInfo.songName}</a></div>
-                           <div class="subtitle is-6">by {$request.leaderboardInfo.songAuthorName}</div>
-                        </div>
-                        <div class="media-right">
-                           <figure class="image is-96x96 mr-0 ml-0">
-                              <img src={$request.leaderboardInfo.coverImage} alt="Map Cover" class="map-cover" />
-                           </figure>
-                        </div>
-                     </div>
-
-                     <div class="content">
-                        Mapped by <a href={'#'}><b>{$request.leaderboardInfo.levelAuthorName}</b></a><br />
-                        Status: <strong>{getRankingApprovalStatus($request.approved)}</strong><br />
-                        Request Type: <strong>{$request.requestType == 1 ? 'Rank' : 'Unrank'}</strong><br />
-                        <hr />
-                        <div class="votes">
-                           <div class="vote">RT 👍<br /><b>{$request.rankVotes.upvotes}</b></div>
-                           <div class="vote">RT 👎<br /><b>{$request.rankVotes.downvotes}</b></div>
-                        </div>
-                        <div class="votes">
-                           <div class="vote">QAT 👍<br /><b>{$request.qatVotes.upvotes}</b></div>
-                           <div class="vote">QAT 😐<br /><b>{$request.qatVotes.neutral}</b></div>
-                           <div class="vote">QAT 👎<br /><b>{$request.qatVotes.downvotes}</b></div>
                         </div>
                      </div>
                   </div>
