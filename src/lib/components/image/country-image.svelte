ac15946edfb4307ca6bf5154f468f513e3e07142
umbranoxio <itsumbra@protonmail.com>
2021-11-05 02:13:50 +1100

    Add twemoji

--- src/lib/components/image/country-image.svelte
+++ src/lib/components/image/country-image.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
-   import { getCDNUrl } from '$lib/utils/helpers';
    import { getName } from 'country-list';
+
    export let country: string;
-   $: countryImagePath = `/flags/${country.toLowerCase()}.png`;
+   const toTwemojiFlag = (input: string) =>
+      `https://twemoji.maxcdn.com/v/13.1.0/svg/${input
+         .toLowerCase()
+         .match(/[a-z]/g)
+         .map((i) => (i.codePointAt(0) + 127365).toString(16))
+         .join('-')}.svg`;
+
+   $: countryImage = toTwemojiFlag(country);
 </script>
 
-<img alt={getName(country)} title={getName(country)} src={getCDNUrl(countryImagePath)} class="is-country" />
+<img alt={getName(country)} title={getName(country)} src={countryImage} class="country" />
+
+<style>
+   .country {
+      cursor: help;
+      width: 18px;
+      height: 18px;
+   }
+</style>
