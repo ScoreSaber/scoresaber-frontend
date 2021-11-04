ec80a8df7cd0a628e8cfd28d5408a314f39b9017
Dannypoke03 <daniel.carruthers@outlook.com>
2021-11-04 23:24:48 +1100

    bulma input bad

---
+++ src/lib/components/common/text-input.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+   export let placeholder: string = '';
+   export let value: string = '';
+</script>
+
+<input type="text" bind:value {placeholder} />
+
+<style>
+   input {
+      background-color: transparent;
+      border: none;
+      color: inherit;
+      border-bottom: 1px solid;
+      width: 100%;
+   }
+   input:focus {
+      outline: none;
+      border-bottom: 2px solid #fff;
+   }
+</style>
