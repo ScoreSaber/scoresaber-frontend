12cb6f807a36f2ac44e9d96046ee036770fb6418
umbranoxio <itsumbra@protonmail.com>
2021-11-07 06:34:05 +1100

    Fix country images

--- src/lib/components/image/country-image.svelte
+++ src/lib/components/image/country-image.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
+   import { getCDNUrl } from '$lib/utils/helpers';
    import { getName } from 'country-list';
-
+   import { onMount } from 'svelte';
    export let country: string;
-   const toTwemojiFlag = (input: string) =>
-      `https://twemoji.maxcdn.com/v/13.1.0/svg/${input
-         .toLowerCase()
-         .match(/[a-z]/g)
-         .map((i) => (i.codePointAt(0) + 127365).toString(16))
-         .join('-')}.svg`;
-   const toEmojiFlag = (countryCode: string): string =>
-      countryCode.toLowerCase().replace(/[a-z]/g, (i) => String.fromCodePoint((i.codePointAt(0) ?? 0) - 97 + 0x1f1e6));
 
-   $: countryImage = toTwemojiFlag(country);
-   $: countryEmoji = toEmojiFlag(country);
+   $: countryImage = getCDNUrl('/flags/unknown.png');
+   $: countryEmoji = 'Unknown';
+
+   onMount(() => {
+      if (country != '') {
+         const toTwemojiFlag = (input: string) =>
+            `https://twemoji.maxcdn.com/v/13.1.0/svg/${input
+               .toLowerCase()
+               .match(/[a-z]/g)
+               .map((i) => (i.codePointAt(0) + 127365).toString(16))
+               .join('-')}.svg`;
+         const toEmojiFlag = (countryCode: string): string =>
+            countryCode.toLowerCase().replace(/[a-z]/g, (i) => String.fromCodePoint((i.codePointAt(0) ?? 0) - 97 + 0x1f1e6));
+
+         countryImage = toTwemojiFlag(country);
+         countryEmoji = toEmojiFlag(country);
+      }
+   });
 </script>
 
 <img alt={countryEmoji} title={getName(country)} src={countryImage} class="country" />
