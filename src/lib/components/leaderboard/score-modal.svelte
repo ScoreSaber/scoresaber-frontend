1afc8d5e11dc66a57b376c362a26a764b162cfa7
JiveOff <antoine@jiveoff.fr>
2021-11-10 11:34:50 +0100

    score details peepoBlush

--- src/lib/components/leaderboard/score-modal.svelte
+++ src/lib/components/leaderboard/score-modal.svelte
@@ -33,16 +33,18 @@
    };
 </script>
 
-<div class="modal {isVisible ? 'is-active' : ''}">
+<div class="modal {isVisible ? 'is-active' : ''} modal-radius">
    <div class="modal-background" />
    <div class="modal-content">
-      <div class="card map-card" style="background-color: var(--gray)">
+      <div class="card map-card">
          <div
             class="bg-image"
             style={`background: linear-gradient(to left, rgba(36, 36, 36, 0.93), rgb(33, 33, 33)) repeat scroll 0% 0%, rgba(0, 0, 0, 0) url(${leaderboard.coverImage}) repeat scroll 0% 0%`}
          />
          <div class="card-content">
             <div class="content">
+               <button class="modal-close is-large" aria-label="close" on:click={() => (isVisible = false)} />
+
                <div class="media">
                   <div class="media-content is-clipped">
                      <div class="player-info mb-2">
@@ -53,49 +55,88 @@
                            class="image rounded is-24x24"
                         />
                         <b><PlayerLink player={score.leaderboardPlayerInfo} destination={`/u/${score.leaderboardPlayerInfo.id}`} /></b>
-                        <span><FormattedDate date={new Date(score.timeSet)} /></span>
+                        <span class="text-muted"><FormattedDate date={new Date(score.timeSet)} /> on <b>{HMDs[score.hmd]}</b></span>
                      </div>
-                     Score:
-                     <b>{score.modifiedScore.toLocaleString('en-US')}</b><br />
-                     {#if leaderboard.maxScore}
-                        Accuracy:
-                        <b class={new Date(score.timeSet).getTime() / 1000 <= 1558474032 ? 'old-score' : ''}>{getAccuracy().toFixed(2)}%</b><br />
-                     {/if}
-                     {#if leaderboard.ranked}
-                        PP: <b
-                           ><span title="Performance Points" class="pp"
-                              >{score.pp.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span
-                           ><span class="pp ppLabel">pp</span></b
-                        ><br />
-                     {/if}<br />
-                     {#if otherScores.filter((score) => score.modifiers.length > 0).length > 0}
-                        Mods:
-                        <b>{score.modifiers}</b> (x{score.multiplier})<br />
-                     {/if}
-                     Full Combo: <b><i class="fas {score.fullCombo ? 'fa-check' : 'fa-times'}" /></b><br />
-                     {#if !score.fullCombo}
-                        Max Combo: <b>{score.maxCombo}</b><br />
-                        Bad Cuts: <b>{score.badCuts}</b><br />
-                        Missed Notes: <b>{score.missedNotes}</b><br />
-                     {/if}<br />
-                     HMD: <b>{HMDs[score.hmd]}</b><br />
-                  </div>
-                  <div class="media-right">
-                     <figure class="image is-96x96 mr-0 ml-0">
-                        <img src={leaderboard.coverImage} alt="Map Cover" class="map-cover" />
-                     </figure>
+                     <div class="scores">
+                        <div class="score">Score<br /><b>{score.modifiedScore.toLocaleString('en-US')}</b></div>
+                        {#if leaderboard.maxScore}
+                           <div class="score {new Date(score.timeSet).getTime() / 1000 <= 1558474032 ? 'old-score' : ''}">
+                              Accuracy<br /><b>{getAccuracy().toFixed(2)}%</b>
+                           </div>
+                        {/if}
+                        {#if leaderboard.ranked}
+                           <div class="score" style="background-color: var(--ppColour)">
+                              PP<br /><b
+                                 ><span title="Performance Points"
+                                    >{score.pp.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span
+                                 ><span class="ppLabel">pp</span></b
+                              >
+                           </div>
+                        {/if}
+                     </div>
+                     <div class="scores">
+                        {#if otherScores.filter((score) => score.modifiers.length > 0).length > 0}
+                           <div class="score">
+                              Mods<br /><b>{score.modifiers}</b> (x{score.multiplier})
+                           </div>
+                        {/if}
+                        <div class="score">
+                           Full Combo<br /><b><i class="fas {score.fullCombo ? 'fa-check' : 'fa-times'}" /></b>
+                        </div>
+                        {#if !score.fullCombo}
+                           <div class="score">
+                              Max Combo<br /><b>{score.maxCombo}</b>
+                           </div>
+                           <div class="score">
+                              Bad Cuts<br /><b>{score.badCuts}</b>
+                           </div>
+                           <div class="score">
+                              Missed Notes<br /><b>{score.missedNotes}</b>
+                           </div>
+                        {/if}
+                     </div>
                   </div>
                </div>
             </div>
          </div>
       </div>
    </div>
-   <button class="modal-close is-large" aria-label="close" on:click={() => (isVisible = false)} />
 </div>
 
 <style>
+   .text-muted {
+      color: var(--muted);
+   }
+
+   .scores {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      align-items: center;
+      margin-top: 0.8rem;
+      text-align: center;
+      gap: 0.5rem;
+   }
+
+   .score {
+      background-color: var(--foregroundItem);
+      padding: 0.2rem 0.3rem;
+      border-radius: 5px;
+      flex-grow: 1;
+   }
+
    .old-score {
-      color: red;
+      background-color: rgb(163, 0, 0);
+   }
+
+   .modal-radius {
+      border-radius: 0.5rem;
+   }
+
+   .modal-close {
+      position: absolute;
+      right: 1rem;
+      top: 1rem;
    }
 
    .player-info {
@@ -116,7 +157,11 @@
       background-repeat: no-repeat !important;
       background-size: cover !important;
       z-index: -1;
-      border-radius: 5px;
+      border-radius: 10px;
+   }
+
+   .card {
+      background: none;
    }
 
    .map-card {
