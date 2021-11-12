c5bc8c4e054fe69398902e11e36d91d98892ebae
umbranoxio <itsumbra@protonmail.com>
2021-11-12 18:19:55 +1100

    Add key based authorization

--- src/lib/utils/fetcher.ts
+++ src/lib/utils/fetcher.ts
@@ -1,12 +1,18 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { dev } from '$app/env';
-import { API_URL } from './env';
+import { API_URL, API_KEY } from './env';
 
 export default async function <T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+   const isAPI = url.startsWith('/api');
+
    if (dev) {
-      if (url.startsWith('/api')) {
+      if (isAPI) {
          url = `${API_URL}${url}`;
       }
+   }
+
+   if (isAPI) {
+      config = { ...config, headers: { Authorization: API_KEY } };
    }
    const response = await axios.get<T>(url, config);
    return response.data;
