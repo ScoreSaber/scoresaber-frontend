b666186ec21dcb16de19d36647c6042b4cc31443
JiveOff <antoine@jiveoff.fr>
2021-11-17 13:57:07 +0100

    little styling change

--- src/routes/ranking/requests.svelte
+++ src/routes/ranking/requests.svelte
@@ -74,7 +74,7 @@
       <div class="window has-shadow">
          {#if $topRequests}
             <div in:fly={{ x: 20, duration: 1000 }}>
-               <h3>Next items in queue</h3>
+               <h3 style="margin-bottom: -1rem;">Next items in queue</h3>
                <div class="ranking">
                   <table>
                      <thead>
