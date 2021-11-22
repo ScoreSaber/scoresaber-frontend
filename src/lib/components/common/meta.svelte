c09efc2d14680da1e7c60b32bbf25f05f2afa5ce
umbranoxio <itsumbra@protonmail.com>
2021-11-22 19:48:29 +1100

    Fix new lines on metatags

--- src/lib/components/common/meta.svelte
+++ src/lib/components/common/meta.svelte
@@ -3,12 +3,13 @@
    export let title: string = 'ScoreSaber';
    export let description: string = 'ScoreSaber';
    export let url: string = 'https://new.scoresaber.com';
+   let descriptionTag = `<meta property="og:description" content=${description} />`;
 </script>
 
 <meta property="og:image" content={image} />
 <meta property="og:image:secure_url" content={title} />
 <meta property="og:title" content={title} />
-<meta property="og:description" content={description} />
+{@html descriptionTag}
 <meta property="og:type" content="website" />
 <meta property="og:url" content={url} />
 <meta property="og:site_name" content="ScoreSaber" />
