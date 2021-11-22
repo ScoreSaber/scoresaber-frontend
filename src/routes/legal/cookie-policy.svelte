e8bd8e2fc182310cbc1daf15a307d2e47d95003e
umbranoxio <itsumbra@protonmail.com>
2021-11-22 17:02:03 +1100

    Add cookie policy

---
+++ src/routes/legal/cookie-policy.svelte
@@ -0,0 +1,135 @@
+<div class="section">
+   <div class="container">
+      <div class="window has-shadow">
+         <div class="content">
+            <p class="has-line-data" data-line-start="1" data-line-end="2">Cookies Policy</p>
+            <p class="has-line-data" data-line-start="3" data-line-end="4">Last updated: February 10, 2020</p>
+            <p class="has-line-data" data-line-start="5" data-line-end="6">
+               This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of
+               cookies We use, or the information We collect using Cookies and how that information is used.
+            </p>
+            <p class="has-line-data" data-line-start="7" data-line-end="8">
+               Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You
+               may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your
+               personal data secure, see our Privacy Policy.
+            </p>
+            <p class="has-line-data" data-line-start="9" data-line-end="10">
+               We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.
+            </p>
+            <h2 class="code-line" data-line-start="11" data-line-end="12">
+               <a id="Interpretation_and_Definitions_11" />Interpretation and Definitions
+            </h2>
+            <h3 class="code-line" data-line-start="13" data-line-end="14"><a id="Interpretation_13" />Interpretation</h3>
+            <p class="has-line-data" data-line-start="15" data-line-end="16">
+               The words of which the initial letter is capitalized have meanings defined under the following conditions.
+            </p>
+            <p class="has-line-data" data-line-start="17" data-line-end="18">
+               The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+            </p>
+            <h3 class="code-line" data-line-start="19" data-line-end="20"><a id="Definitions_19" />Definitions</h3>
+            <p class="has-line-data" data-line-start="21" data-line-end="22">For the purposes of this Cookies Policy:</p>
+            <ul>
+               <li class="has-line-data" data-line-start="23" data-line-end="24">
+                  <strong>Company</strong> (referred to as either “the Company”, “We”, “Us” or “Our” in this Cookies Policy) refers to Score Saber.
+               </li>
+               <li class="has-line-data" data-line-start="24" data-line-end="25">
+                  <strong>You</strong> means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual
+                  is accessing or using the Website, as applicable.
+               </li>
+               <li class="has-line-data" data-line-start="25" data-line-end="26">
+                  <strong>Cookies</strong> means small files that are placed on Your computer, mobile device or any other device by a website, containing
+                  details of your browsing history on that website among its many uses.
+               </li>
+               <li class="has-line-data" data-line-start="26" data-line-end="27">
+                  <strong>Website</strong> refers to Score Saber, accessible from <a href="http://www.scoresaber.com">www.scoresaber.com</a>.
+               </li>
+            </ul>
+            <h2 class="code-line" data-line-start="29" data-line-end="30"><a id="The_use_of_the_Cookies_29" />The use of the Cookies</h2>
+            <h3 class="code-line" data-line-start="31" data-line-end="32"><a id="Type_of_Cookies_We_Use_31" />Type of Cookies We Use</h3>
+            <p class="has-line-data" data-line-start="33" data-line-end="34">
+               Cookies can be “Persistent” or “Session” Cookies. Persistent Cookies remain on your personal computer or mobile device when You go
+               offline, while Session Cookies are deleted as soon as You close your web browser.
+            </p>
+            <p class="has-line-data" data-line-start="35" data-line-end="36">
+               We use both session and persistent Cookies for the purposes set out below:
+            </p>
+            <p class="has-line-data" data-line-start="37" data-line-end="41">
+               <strong>Necessary / Essential Cookies</strong><br />
+               Type: Session Cookies<br />
+               Administered by: Us<br />
+               <strong>Purpose:</strong> These Cookies are essential to provide You with services available through the Website and to enable You to use
+               some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that
+               You have asked for cannot be provided, and We only use these Cookies to provide You with those services.
+            </p>
+            <p class="has-line-data" data-line-start="42" data-line-end="46">
+               <strong>Functionality Cookies</strong><br />
+               Type: Persistent Cookies<br />
+               Administered by: Us<br />
+               <strong>Purpose:</strong> These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details
+               or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter
+               your preferences every time You use the Website.
+            </p>
+            <h3 class="code-line" data-line-start="47" data-line-end="48">
+               <a id="Your_Choices_Regarding_Cookies_47" />Your Choices Regarding Cookies
+            </h3>
+            <p class="has-line-data" data-line-start="49" data-line-end="50">
+               If You prefer to avoid the use of Cookies on the Website, first You must disable the use of Cookies in your browser and then delete the
+               Cookies saved in your browser associated with this website. You may use this option for preventing the use of Cookies at any time.
+            </p>
+            <p class="has-line-data" data-line-start="51" data-line-end="52">
+               If You do not accept Our Cookies, You may experience some inconvenience in your use of the Website and some features may not function
+               properly.
+            </p>
+            <p class="has-line-data" data-line-start="53" data-line-end="54">
+               If You’d like to delete Cookies or instruct your web browser to delete or refuse Cookies, please visit the help pages of your web
+               browser.
+            </p>
+            <ul>
+               <li class="has-line-data" data-line-start="55" data-line-end="56">
+                  For the Chrome web browser, please visit this page from Google:
+                  <a href="https://support.google.com/accounts/answer/32050">https://support.google.com/accounts/answer/32050</a>
+               </li>
+               <li class="has-line-data" data-line-start="56" data-line-end="57">
+                  For the Internet Explorer web browser, please visit this page from Microsoft:
+                  <a href="https://support.microsoft.com/kb/278835">http://support.microsoft.com/kb/278835</a>
+               </li>
+               <li class="has-line-data" data-line-start="57" data-line-end="58">
+                  For the Firefox web browser, please visit this page from Mozilla:
+                  <a href="https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored"
+                     >https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored</a
+                  >
+               </li>
+               <li class="has-line-data" data-line-start="58" data-line-end="60">
+                  For the Safari web browser, please visit this page from Apple:
+                  <a href="https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac"
+                     >https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac</a
+                  >
+               </li>
+            </ul>
+            <p class="has-line-data" data-line-start="60" data-line-end="61">
+               For any other web browser, please visit your web browser’s official web pages.
+            </p>
+            <h3 class="code-line" data-line-start="62" data-line-end="63">
+               <a id="More_Information_about_Cookies_62" />More Information about Cookies
+            </h3>
+            <p class="has-line-data" data-line-start="64" data-line-end="65">
+               You can learn more about Cookies at the following third-party websites:
+            </p>
+            <ul>
+               <li class="has-line-data" data-line-start="66" data-line-end="67">
+                  Network Advertising Initiative: <a href="https://www.networkadvertising.org/">http://www.networkadvertising.org/</a>
+               </li>
+            </ul>
+            <h3 class="code-line" data-line-start="69" data-line-end="70"><a id="Contact_Us_69" />Contact Us</h3>
+            <p class="has-line-data" data-line-start="71" data-line-end="72">
+               If you have any questions about this Cookies Policy, You can contact us:
+            </p>
+            <ul>
+               <li class="has-line-data" data-line-start="339" data-line-end="340">
+                  By email: <a href="mailto:privacy@scoresaber.com">privacy@scoresaber.com</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</div>
