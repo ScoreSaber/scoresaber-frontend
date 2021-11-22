9e8f004b9c4402b84259ce085a1365f6b9ff6385
Dannypoke03 <daniel.carruthers@outlook.com>
2021-11-22 21:06:31 +1100

    spinny burger

--- src/lib/components/common/navbar.svelte
+++ src/lib/components/common/navbar.svelte
@@ -78,7 +78,11 @@
 <div bind:this={scrollProbe} />
 <header class="{isExpanded ? 'expanded' : ''} {headerIncreaseContrast ? 'scrolled' : ''}" bind:this={header}>
    <div class="container">
-      <button class="hamburger" on:click={() => (isExpanded = !isExpanded)}><i class="fa fa-bars" /></button>
+      <div class="hamburger hamburger--spin js-hamburger" on:click={() => (isExpanded = !isExpanded)} class:is-active={isExpanded}>
+         <div class=" hamburger-box ">
+            <div class="hamburger-inner " />
+         </div>
+      </div>
       <nav>
          <a on:click={handleClick} href="/" class="logo-container"
             ><img src="/images/logo.svg" class="logo" alt="ScoreSaber" /><span class="navbar-label"><strong>ScoreSaber</strong></span></a
@@ -183,8 +187,7 @@
       padding: 15px;
    }
 
-   nav > a.square,
-   .hamburger {
+   nav > a.square {
       width: 41px;
       padding: 0;
       align-items: center;
@@ -297,6 +300,108 @@
    .fake-searchbox kbd {
       padding: 2px 4px;
       color: #fff;
+   }
+
+   .hamburger {
+      padding: 15px 15px;
+      display: inline-block;
+      cursor: pointer;
+      transition-property: opacity, filter;
+      transition-duration: 0.15s;
+      transition-timing-function: linear;
+      font: inherit;
+      color: inherit;
+      text-transform: none;
+      background-color: transparent;
+      border: 0;
+      margin: 0;
+      overflow: visible;
+      scale: 0.8;
+   }
+
+   .hamburger:hover {
+      opacity: 0.7;
+   }
+
+   .hamburger.is-active:hover {
+      opacity: 0.7;
+   }
+
+   .hamburger.is-active .hamburger-inner,
+   .hamburger.is-active .hamburger-inner::before,
+   .hamburger.is-active .hamburger-inner::after {
+      background-color: rgba(255, 255, 255, 0.5);
+   }
+
+   .hamburger-box {
+      width: 40px;
+      height: 24px;
+      display: inline-block;
+      position: relative;
+   }
+
+   .hamburger-inner {
+      display: block;
+      top: 50%;
+      margin-top: -2px;
+   }
+
+   .hamburger-inner,
+   .hamburger-inner::before,
+   .hamburger-inner::after {
+      width: 40px;
+      height: 4px;
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 4px;
+      position: absolute;
+      transition-property: transform;
+      transition-duration: 0.15s;
+      transition-timing-function: ease;
+   }
+
+   .hamburger-inner::before,
+   .hamburger-inner::after {
+      content: '';
+      display: block;
+   }
+
+   .hamburger-inner::before {
+      top: -10px;
+   }
+
+   .hamburger-inner::after {
+      bottom: -10px;
+   }
+
+   .hamburger--spin .hamburger-inner {
+      transition-duration: 0.22s;
+      transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+   }
+
+   .hamburger--spin .hamburger-inner::before {
+      transition: top 0.1s 0.25s ease-in, opacity 0.1s ease-in;
+   }
+
+   .hamburger--spin .hamburger-inner::after {
+      transition: bottom 0.1s 0.25s ease-in, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+   }
+
+   .hamburger--spin.is-active .hamburger-inner {
+      transform: rotate(225deg);
+      transition-delay: 0.12s;
+      transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+   }
+
+   .hamburger--spin.is-active .hamburger-inner::before {
+      top: 0;
+      opacity: 0;
+      transition: top 0.1s ease-out, opacity 0.1s 0.12s ease-out;
+   }
+
+   .hamburger--spin.is-active .hamburger-inner::after {
+      bottom: 0;
+      transform: rotate(-90deg);
+      transition: bottom 0.1s ease-out, transform 0.22s 0.12s cubic-bezier(0.215, 0.61, 0.355, 1);
    }
 
    @media all and (max-width: 720px) {
