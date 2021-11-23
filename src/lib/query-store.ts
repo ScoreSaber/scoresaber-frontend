06454cb05c0e7f8394baee6d6e1d869e4cd83482
Dannypoke03 <daniel.carruthers@outlook.com>
2021-11-23 11:41:53 +1100

    better handling & stronger typings

--- src/lib/query-store.ts
+++ src/lib/query-store.ts
@@ -5,41 +5,45 @@ import queryString from 'query-string';
 
 export const queryChangeTimeout = writable(null);
 
-export function pageQueryStore<T extends object, Key extends keyof T>(props: T) {
-   var query = undefined;
-   return {
-      subscribe: (h): T => {
-         return page.subscribe((p) => {
-            query = queryString.parse(p.query.toString(), { parseNumbers: true });
-            let newParams: T = props;
-            for (let key in query) {
-               if (key in props) newParams[key] = query[key];
-            }
-            for (const key in props) {
-               if (!(key in query)) newParams[key] = null;
-            }
-            h(newParams);
-         });
-      },
-      updateSingle: async (prop: Key, v: any): Promise<void> => {
-         if (v === null) {
-            delete query[prop];
-         } else {
-            query[prop] = v;
+export function pageQueryStore<T extends object>(props: T) {
+   let queryStore = writable<Partial<T>>(props);
+   let initialLoad = true;
+   page.subscribe((p) => {
+      let query = queryString.parse(p.query.toString(), { parseNumbers: true });
+      let newParams: T = props;
+      for (let key in query) {
+         if (key in props) newParams[key] = query[key];
+      }
+      // for the first load keep initial prop values i.e. page = 1
+      if (!initialLoad) {
+         for (const key in props) {
+            if (!(key in query)) newParams[key] = null;
          }
-         const urlSearchParams = new URLSearchParams(query);
+      }
+      if (initialLoad) initialLoad = false;
+      queryStore.set(newParams);
+   });
+   // removes null values from query string
+   const queryFilter = (query: Partial<T>) => {
+      return Object.keys(query).filter((key) => query[key] !== null).map((key) => {
+         return { [key]: query[key] };
+      }).reduce((a, b) => ({ ...a, ...b }), {});
+   }
+   return {
+      subscribe: queryStore.subscribe,
+      updateSingle: async (prop: keyof T, v: any): Promise<void> => {
+         let query = get(queryStore);
+         query[prop] = v;
+         const urlSearchParams = new URLSearchParams(queryFilter(query));
          const g = `?${urlSearchParams.toString()}`;
          goto(g, { keepfocus: true, noscroll: true });
       },
       update: async (props: Partial<T>): Promise<void> => {
+         let query = get(queryStore);
          Object.keys(props).forEach((p) => {
-            if (props[p] === null) {
-               delete query[p];
-            } else {
-               query[p] = props[p];
-            }
+            query[p] = props[p];
          });
-         const urlSearchParams = new URLSearchParams(query);
+         const urlSearchParams = new URLSearchParams(queryFilter(query));
          const g = `?${urlSearchParams.toString()}`;
          goto(g, { keepfocus: true, noscroll: true });
       }
