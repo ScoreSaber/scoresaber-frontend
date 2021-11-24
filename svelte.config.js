1ed12f679c47b812aaef760034c27a6f24b08d54
umbranoxio <itsumbra@protonmail.com>
2021-11-25 03:19:19 +1100

    Add markdown support

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+   extensions: ['.svelte', ...mdsvexConfig.extensions],
+
    // Consult https://github.com/sveltejs/svelte-preprocess
    // for more information about preprocessors
-   preprocess: preprocess(),
+   preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
    kit: {
       //   ssr: false,
