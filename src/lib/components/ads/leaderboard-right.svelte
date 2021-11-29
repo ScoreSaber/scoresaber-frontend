46875ea31c894c4e68273f632007703b87fbda09
umbranoxio <itsumbra@protonmail.com>
2021-11-29 12:17:39 +1100

    Only display on desktop

--- src/lib/components/ads/leaderboard-right.svelte
+++ src/lib/components/ads/leaderboard-right.svelte
@@ -8,12 +8,22 @@
    });
 </script>
 
-<ins
-   class="adsbygoogle"
-   style="display:block"
-   data-ad-format="fluid"
-   data-ad-layout-key="-fg-1j+1l-o1+1ae"
-   data-ad-client="ca-pub-9829527932347716"
-   data-ad-slot="6830139879"
-   data-adtest={dev ? 'on' : 'off'}
-/>
+<div class="ad">
+   <ins
+      class="adsbygoogle"
+      style="display:block"
+      data-ad-format="fluid"
+      data-ad-layout-key="-fg-1j+1l-o1+1ae"
+      data-ad-client="ca-pub-9829527932347716"
+      data-ad-slot="6830139879"
+      data-adtest={dev ? 'on' : 'off'}
+   />
+</div>
+
+<style>
+   @media only screen and (max-width: 769px) {
+      .ad {
+         display: none;
+      }
+   }
+</style>
