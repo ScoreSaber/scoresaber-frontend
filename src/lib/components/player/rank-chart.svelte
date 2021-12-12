ee926fe2e2c153332a18fb660b5e2caad6e144b5
Umbranox <itsumbra@protonmail.com>
2021-12-12 15:23:41 +1100

    uhh

--- src/lib/components/player/rank-chart.svelte
+++ src/lib/components/player/rank-chart.svelte
@@ -15,6 +15,53 @@
       window.addEventListener('resize', () => {
          options = getOptions();
       });
+      if (player.id == '76561198064659288') {
+         const chartCanvas = document.getElementById('rank-chart').firstElementChild as HTMLElement;
+
+         const denyahCount = 10;
+         const denyahSections = history.length / denyahCount;
+         const nums: number[] = [];
+         for (let i = 0; i < history.length; i += denyahSections) {
+            let sum = 0;
+            for (let x = i; x < i + denyahSections; x++) {
+               sum += history[x];
+            }
+            nums.push(sum / denyahSections);
+         }
+
+         let totalSum = 0;
+
+         for (let i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+         }
+
+         let trueAverage = totalSum / nums.length;
+         const denyahs: string[] = [];
+         const backgroundPositions: string[] = [];
+         const backgroundAlignments: string[] = [];
+         const backgroundWidth: number = 100 / nums.length;
+         for (let i = 0; i < nums.length; i++) {
+            if (i == 0) {
+               if (nums[i] < trueAverage) {
+                  denyahs.push('url(/images/denyah-good.png)');
+               } else {
+                  denyahs.push('url(/images/denyah-bad.png)');
+               }
+            } else {
+               if (nums[i - 1] > nums[i]) {
+                  denyahs.push('url(/images/denyah-good.png)');
+               } else {
+                  denyahs.push('url(/images/denyah-bad.png)');
+               }
+            }
+            backgroundPositions.push(`${(i / (nums.length - 1)) * 100}%`);
+         }
+         chartCanvas.style.backgroundImage = denyahs.join(', ');
+         chartCanvas.style.backgroundRepeat = 'no-repeat';
+         chartCanvas.style.backgroundPositionX = backgroundPositions.join(', ');
+         chartCanvas.style.backgroundSize = `${backgroundWidth + 1}% 100%`;
+         chartCanvas.style.borderRadius = '5px';
+      }
    });
 
    const getXTickLimit = (): number => {
@@ -54,7 +101,8 @@
             data: history,
             label: 'Rank',
             borderColor: '#3e95cd',
-            fill: false
+            fill: false,
+            color: '#fff'
          }
       ]
    };
@@ -105,7 +153,7 @@
    }
 </script>
 
-<div class="rank-chart">
+<div id="rank-chart" class="rank-chart">
    <Line bind:options {data} />
 </div>
 
