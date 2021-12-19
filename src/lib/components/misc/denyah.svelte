4ff6f020ae4f7ffb1d1b7faa3fa539efabcc6a7f
Rain <96358516+InkieRain@users.noreply.github.com>
2021-12-19 11:01:54 +1100

    bonk

--- src/lib/components/misc/denyah.svelte
+++ src/lib/components/misc/denyah.svelte
@@ -9,6 +9,11 @@
          if (playerId == '76561198064659288') {
             page.style.transform = 'rotateZ(8deg)';
             page.style.filter = 'blur(1px) hue-rotate(180deg)';
+            setInterval(async () => {
+               page.style.transform = 'rotateZ(188deg)';
+               await new Promise((resolve) => setTimeout(resolve, 2000));
+               page.style.transform = 'rotateZ(8deg)';
+            }, 13000);
          } else {
             page.style.transform = 'rotateZ(0deg)';
             page.style.filter = 'blur(0px)';
