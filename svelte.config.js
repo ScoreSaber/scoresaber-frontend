c8df2a22419e4ed0b3f35747b4896357dbe2658c
Umbranox <itsumbra@protonmail.com>
2022-09-13 04:56:48 +1000

    Back to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 import mdsvexConfig from './mdsvex.config.js';
 
