8fe087994f01629b8ebe40c6d3bb696a547849e8
skearya <77034153+skearya@users.noreply.github.com>
2023-02-23 15:44:57 -0500

    Update denyah.svelte

--- src/lib/components/misc/denyah.svelte
+++ src/lib/components/misc/denyah.svelte
@@ -1,25 +1,33 @@
 <script lang="ts">
-   import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+ 
+    export let playerId: string;
+ 
+    onMount(() => {
+        denyah()
+    });
 
-   export let playerId: string;
+    $: $page.path, denyah()
 
-   onMount(() => {
-      const page = document.getElementById('page');
-      if (page) {
-         if (playerId == '76561198064659288') {
-            page.style.transform = 'rotateZ(8deg)';
-            page.style.filter = 'blur(1px) hue-rotate(180deg)';
-            page.style.cursor = 'wait';
+    function denyah() {
+        const page = document.getElementById("page");
+        if (page) {
+          if (playerId == "76561198064659288") {
+            page.style.transform = "rotateZ(8deg)";
+            page.style.filter = "blur(1px) hue-rotate(180deg)";
+            page.style.cursor = "wait";
             setInterval(async () => {
-               page.style.transform = 'rotateZ(188deg)';
-               await new Promise((resolve) => setTimeout(resolve, 2000));
-               page.style.transform = 'rotateZ(8deg)';
+              page.style.transform = "rotateZ(188deg)";
+              await new Promise((resolve) => setTimeout(resolve, 2000));
+              page.style.transform = "rotateZ(8deg)";
             }, 13000);
-         } else {
-            page.style.transform = 'rotateZ(0deg)';
-            page.style.filter = 'blur(0px)';
-            page.style.cursor = 'default';
-         }
-      }
-   });
-</script>
+          } else {
+            page.style.transform = "rotateZ(0deg)";
+            page.style.filter = "blur(0px)";
+            page.style.cursor = "default";
+          }
+        }
+      }      
+ </script>
+ 
