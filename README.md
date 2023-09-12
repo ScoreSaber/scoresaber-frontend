9d64f883117c0950879733cc69ac0880f4065b5e
Umbranox <itsumbra@protonmail.com>
2023-09-13 04:56:02 +1000

    Dynamic env

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Once you've cloned the project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), create a new `.env` file using the template from `.env.example` CORS is currently enabled for both the CDN and the API so your `.env` should look a little like this until we find a better solution in the future
 
 ```
-VITE_CDN_URL=https://cdn.scoresaber.com
-VITE_API_URL=https://scoresaber.com
+PUBLIC_CDN_URL=https://cdn.scoresaber.com
+PUBLIC_API_URL=https://scoresaber.com
 ```
 
 To start a development server:
--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,5 +1,6 @@
-export const CDN_URL: string = import.meta.env.VITE_CDN_URL as string;
-export const API_URL: string = (import.meta.env.VITE_API_URL as string) ?? '';
-export const API_KEY: string = import.meta.env.VITE_API_KEY as string;
-export const CACHE_EXPIRY_IN_MINUTES: string = import.meta.env.VITE_CACHE_EXPIRY_IN_MINUTES as string;
+import { env } from '$env/dynamic/public';
 
+export const CDN_URL: string = env.PUBLIC_CDN_URL as string;
+export const API_URL: string = (env.PUBLIC_API_URL as string) ?? '';
+export const API_KEY: string = env.PUBLIC_API_KEY as string;
+export const CACHE_EXPIRY_IN_MINUTES: string = env.PUBLIC_CACHE_EXPIRY_IN_MINUTES as string;
