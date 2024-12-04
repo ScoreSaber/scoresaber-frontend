ee40f0a8a28cce983690e3d496a64cae13b5cb7f
George Fischer <me@george.id.au>
2024-12-05 00:12:44 +1100

    Use presence information for online status

--- src/lib/stores/presence-store.ts
+++ src/lib/stores/presence-store.ts
@@ -1,16 +1,21 @@
 import type { Readable } from 'svelte/store';
 
-import socket, { GameMode, type LiveSongInfo, type PauseUnpauseEvent } from '$lib/utils/presence-socket';
+import { Presence } from 'phoenix';
+
+import socket, { type LiveSongInfo, type PauseUnpauseEvent } from '$lib/utils/presence-socket';
+
 
 export enum Scene {
    Offline = 'offline',
    Menu = 'menu',
+   Online = 'online',
    Playing = 'playing'
 }
 
 export type UserState =
    | null
    | { scene: Scene.Offline }
+   | { scene: Scene.Online }
    | { scene: Scene.Menu }
    | { scene: Scene.Playing; currentMap: LiveSongInfo; lastPause?: PauseUnpauseEvent };
 
@@ -20,7 +25,26 @@ export const userPresence = (userId: bigint | string) =>
          const channel = socket.channel(`user_profile:${userId}`);
          channel.join();
 
-         cb(null);
+         const presence = new Presence(channel);
+
+         let state: UserState = null;
+
+         presence.onJoin((id) => {
+            if(id === userId && (!state || state.scene === Scene.Offline)) {
+               state = {scene: Scene.Online};
+               console.log("join", state);
+               cb(state);
+            }
+         });
+         presence.onLeave(id => {
+            if(id == userId) {
+               state = {scene: Scene.Offline};
+               console.log("leave", state);
+               cb(state);
+            }
+         });
+
+         cb(state);
 
          channel.on('state_changed', (v) => {
             cb(v);
