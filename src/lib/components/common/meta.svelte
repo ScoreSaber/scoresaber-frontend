b18442cdfbf8019daa2550fb57465e1399fbcc81
Umbranox <root@umbranox.dev>
2025-11-10 19:30:28 +1100

    remove image width / height from meta tags

--- src/lib/components/common/meta.svelte
+++ src/lib/components/common/meta.svelte
@@ -24,8 +24,6 @@
 <meta property="og:description" content={description} />
 <meta property="og:image" content={image} />
 <meta property="og:image:secure_url" content={image} />
-<meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="630" />
 <meta property="og:site_name" content="ScoreSaber" />
 <meta property="og:locale" content="en_US" />
 
