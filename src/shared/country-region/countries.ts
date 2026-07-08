f6a822e4eb06ec93125d3158a001f2aa5c15283b
Umbranox <root@umbranox.dev>
2026-07-09 04:45:06 +1000

    website/regions: align region country filters

--- src/shared/country-region/countries.ts
+++ src/shared/country-region/countries.ts
@@ -32,6 +32,7 @@ export const countryCodeSchema = z.enum([
    'BM',
    'BN',
    'BO',
+   'BQ',
    'BR',
    'BS',
    'BT',
@@ -66,6 +67,7 @@ export const countryCodeSchema = z.enum([
    'EC',
    'EE',
    'EG',
+   'EH',
    'ER',
    'ES',
    'ET',
@@ -229,6 +231,7 @@ export const countryCodeSchema = z.enum([
    'TZ',
    'UA',
    'UG',
+   'UM',
    'US',
    'UY',
    'UZ',
@@ -269,21 +272,53 @@ interface Region {
    code: RegionCode;
    label: string;
    countries: string;
+   legacyCountries?: string[];
 }
 
 export const REGIONS: Region[] = [
-   { code: 'NA', label: 'North America', countries: 'US,CA,MX,GT,BZ,HN,SV,NI,CR,PA,CU,JM,HT,DO,PR,TT,BS,BB,AG,DM,GD,KN,LC,VC' },
-   { code: 'SA', label: 'South America', countries: 'BR,AR,CO,PE,VE,CL,EC,BO,PY,UY,GY,SR,GF' },
+   {
+      code: 'NA',
+      label: 'North America',
+      countries: 'AG,AI,AW,BB,BL,BM,BQ,BS,BZ,CA,CR,CU,CW,DM,DO,GD,GL,GP,GT,HN,HT,JM,KN,KY,LC,MF,MQ,MS,MX,NI,PA,PM,PR,SV,SX,TC,TT,US,VC,VG,VI',
+      legacyCountries: ['US,CA,MX,GT,BZ,HN,SV,NI,CR,PA,CU,JM,HT,DO,PR,TT,BS,BB,AG,DM,GD,KN,LC,VC']
+   },
+   {
+      code: 'SA',
+      label: 'South America',
+      countries: 'AR,BO,BR,CL,CO,EC,FK,GF,GY,PE,PY,SR,UY,VE',
+      legacyCountries: ['BR,AR,CO,PE,VE,CL,EC,BO,PY,UY,GY,SR,GF']
+   },
    {
       code: 'EU',
       label: 'Europe',
-      countries: 'GB,DE,FR,IT,ES,NL,BE,AT,CH,PL,SE,NO,DK,FI,IE,PT,CZ,RO,HU,GR,BG,HR,SK,SI,LT,LV,EE,LU,MT,CY,IS,AL,MK,ME,RS,BA,XK,MD,UA,BY'
+      countries:
+         'AD,AL,AT,AX,BA,BE,BG,BY,CH,CY,CZ,DE,DK,EE,ES,FI,FO,FR,GB,GG,GI,GR,HR,HU,IE,IM,IS,IT,JE,LI,LT,LU,LV,MC,MD,ME,MK,MT,NL,NO,PL,PT,RO,RS,RU,SE,SI,SJ,SK,SM,UA,VA,XK',
+      legacyCountries: [
+         'GB,DE,FR,IT,ES,NL,BE,AT,CH,PL,SE,NO,DK,FI,IE,PT,CZ,RO,HU,GR,BG,HR,SK,SI,LT,LV,EE,LU,MT,CY,IS,AL,MK,ME,RS,BA,XK,MD,UA,BY',
+         'GB,DE,FR,IT,ES,NL,BE,AT,CH,PL,SE,NO,DK,FI,IE,PT,CZ,RO,HU,GR,BG,HR,SK,SI,LT,LV,EE,LU,MT,CY,IS,AL,MK,ME,RS,BA,XK,MD,UA,BY,RU'
+      ]
    },
-   { code: 'AS', label: 'Asia', countries: 'CN,JP,KR,IN,ID,TH,VN,PH,MY,SG,TW,HK,BD,PK,LK,NP,MM,KH,LA,MN,KZ,UZ,TJ,KG,TM,AZ,GE,AM' },
+   {
+      code: 'AS',
+      label: 'Asia',
+      countries:
+         'AE,AF,AM,AZ,BD,BH,BN,BT,CC,CN,CX,GE,HK,ID,IL,IN,IO,IQ,IR,JO,JP,KG,KH,KP,KR,KW,KZ,LA,LB,LK,MM,MN,MO,MV,MY,NP,OM,PH,PK,PS,QA,SA,SG,SY,TH,TJ,TM,TR,TW,UZ,VN,YE',
+      legacyCountries: [
+         'CN,JP,KR,IN,ID,TH,VN,PH,MY,SG,TW,HK,BD,PK,LK,NP,MM,KH,LA,MN,KZ,UZ,TJ,KG,TM,AZ,GE,AM',
+         'CN,JP,KR,IN,ID,TH,VN,PH,MY,SG,TW,HK,BD,PK,LK,NP,MM,KH,LA,MN,KZ,UZ,TJ,KG,TM,AZ,GE,AM,IL'
+      ]
+   },
    {
       code: 'AF',
       label: 'Africa',
-      countries: 'ZA,NG,EG,KE,GH,ET,TZ,UG,DZ,MA,TN,SN,CI,CM,AO,MZ,MG,ZW,ZM,RW,ML,BF,NE,TD,GN,BJ,TG,SL,LR,MR,SO,DJ,ER,GM,CV'
+      countries:
+         'AO,BF,BI,BJ,BW,CD,CF,CG,CI,CM,CV,DJ,DZ,EG,EH,ER,ET,GA,GH,GM,GN,GQ,GW,KE,KM,LR,LS,LY,MA,MG,ML,MR,MU,MW,MZ,NA,NE,NG,RE,RW,SC,SD,SH,SL,SN,SO,SS,ST,SZ,TD,TG,TN,TZ,UG,YT,ZA,ZM,ZW',
+      legacyCountries: ['ZA,NG,EG,KE,GH,ET,TZ,UG,DZ,MA,TN,SN,CI,CM,AO,MZ,MG,ZW,ZM,RW,ML,BF,NE,TD,GN,BJ,TG,SL,LR,MR,SO,DJ,ER,GM,CV']
    },
-   { code: 'OCE', label: 'Oceania', countries: 'AU,NZ,FJ,PG,WS,TO,VU,SB,KI,FM,MH,PW,NR,TV' }
+   {
+      code: 'OCE',
+      label: 'Oceania',
+      countries: 'AS,AU,CK,FJ,FM,GU,KI,MH,MP,NC,NF,NR,NU,NZ,PF,PG,PN,PW,SB,TK,TL,TO,TV,UM,VU,WF,WS',
+      legacyCountries: ['AU,NZ,FJ,PG,WS,TO,VU,SB,KI,FM,MH,PW,NR,TV']
+   }
 ];
